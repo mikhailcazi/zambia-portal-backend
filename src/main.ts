3751933b3b243b57f0d3dc0891ccb7fc95ddb040
Mikhail <mikhailcazi@gmail.com>
2025-12-16 15:47:38 +0100

    Adding Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,16 @@ async function bootstrap() {
     ],
     credentials: true, // if using cookies/auth
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Zambia Green Investment Portal API')
+    .setDescription('This is the documentation for the API')
+    .setVersion('1.0')
+    .addTag('')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
